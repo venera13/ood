556f3779614c4e24f4e09594640f611ad1c35fa2
venera13 <veneralobanova1@gmail.com>
2022-03-17 20:39:41 +0300

    Добавила импорты

--- Observer/WeatherStation/weatherStationDuo.php
+++ Observer/WeatherStation/weatherStationDuo.php
@@ -1,17 +1,26 @@
 <?php
 declare(strict_types=1);
 
+include 'Utils/Arrays.php';
 include 'Data/WeatherInfo.php';
+include 'Data/WeatherDuoInfo.php';
 include 'Data/ObserverData.php';
+include 'Data/WeatherDuoInfoList.php';
+include 'Data/SensorStats.php';
+include 'Domain/WeatherInfoType.php';
 include 'Observable/ObservableInterface.php';
 include 'Observable/Observable.php';
 include 'Observer/ObserverInterface.php';
 include 'WeatherData/WeatherData.php';
+include 'WeatherData/WeatherDataPro.php';
 include 'WeatherData/WeatherDataInside.php';
 include 'WeatherData/WeatherDataOutside.php';
 include 'WeatherDisplay/Display.php';
 include 'WeatherDisplay/StatsDisplay.php';
+include 'WeatherDisplay/ProDisplay.php';
+include 'WeatherDisplay/StatsProDisplay.php';
 include 'WeatherDisplay/StatsCalculator.php';
+include 'WeatherDisplay/StatsWindDirectionCalculator.php';
 
 $weatherDataIn = new WeatherDataInside();
 $weatherDataOut = new WeatherDataOutside();
