f01b51e81046ded0c41ca4ecb995a29f11fd4aab
venera13 <veneralobanova1@gmail.com>
2022-05-26 16:39:20 +0300

    Добавила тесты для проверки программы gumball machine с классами состояний

---
+++ State/Tests/Tests.php
@@ -0,0 +1,261 @@
+<?php
+declare(strict_types=1);
+
+namespace State\Tests;
+
+include '../GumballMachine/GumballMachineInterface.php';
+include '../GumballMachine/GumballMachine.php';
+include '../States/StateInterface.php';
+include '../States/HasQuarterState.php';
+include '../States/NoQuarterState.php';
+include '../States/SoldOutState.php';
+include '../States/SoldState.php';
+include '../NaiveGumBallMachine/NaiveGumBallMachine.php';
+include '../NaiveGumBallMachine/StateTypes.php';
+
+use PHPUnit\Framework\TestCase;
+use State\GumballMachine\GumballMachine;
+use State\States\HasQuarterState;
+use State\States\NoQuarterState;
+use State\States\SoldOutState;
+use State\States\SoldState;
+
+class Tests extends TestCase
+{
+    public function testGumballMachine()
+    {
+        $machine = new GumballMachine(5);
+        $this->assertEquals(true, $machine->getBallCount() === 5);
+    }
+
+    public function testGumballMachineReleaseBall()
+    {
+        $machine = new GumballMachine(5);
+        $machine->releaseBall();
+        $this->assertEquals(true, $machine->getBallCount() === 4);
+    }
+
+    public function testGumballMachineFirstState()
+    {
+        $machine = new GumballMachine();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new SoldOutState($machine);
+
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testGumballMachineFirstState2()
+    {
+        $machine = new GumballMachine(2);
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new NoQuarterState($machine);
+
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testNoQuarterStateInsertQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new NoQuarterState($machine);
+        $state->insertQuarter();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new HasQuarterState($machine);
+
+        $this->expectOutputString("You inserted a quarter<br />");
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testNoQuarterStateEjectQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new NoQuarterState($machine);
+        $state->ejectQuarter();
+        $this->expectOutputString("You haven't inserted a quarter<br />");
+    }
+
+    public function testNoQuarterStateTurnCrank()
+    {
+        $machine = new GumballMachine();
+        $state = new NoQuarterState($machine);;
+        $state->turnCrank();
+        $this->expectOutputString("You turned but there's no quarter<br />");
+    }
+
+    public function testNoQuarterStateDispense()
+    {
+        $machine = new GumballMachine();
+        $state = new NoQuarterState($machine);;
+        $state->dispense();
+        $this->expectOutputString("You need to pay first<br />");
+    }
+
+    public function testNoQuarterStateToString()
+    {
+        $machine = new GumballMachine();
+        $state = new NoQuarterState($machine);
+        $this->assertEquals(true, $state->toString() === "waiting for quarter");
+    }
+
+    public function testHasQuarterStateInsertQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new HasQuarterState($machine);
+        $state->insertQuarter();
+
+        $this->expectOutputString("You can't insert another quarter<br />");
+    }
+
+    public function testHasQuarterStateEjectQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new HasQuarterState($machine);
+        $state->ejectQuarter();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new NoQuarterState($machine);
+
+        $this->expectOutputString("Quarter returned<br />");
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testHasQuarterStateTurnCrank()
+    {
+        $machine = new GumballMachine();
+        $state = new HasQuarterState($machine);;
+        $state->turnCrank();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new SoldState($machine);
+
+        $this->expectOutputString("You turned...<br />");
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testHasQuarterStateDispense()
+    {
+        $machine = new GumballMachine();
+        $state = new HasQuarterState($machine);;
+        $state->dispense();
+        $this->expectOutputString("No gumball dispensed<br />");
+    }
+
+    public function testHasQuarterStateToString()
+    {
+        $machine = new GumballMachine();
+        $state = new HasQuarterState($machine);
+        $this->assertEquals(true, $state->toString() === "waiting for turn of crank");
+    }
+
+    public function testSoldOutStateInsertQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldOutState($machine);
+        $state->insertQuarter();
+
+        $this->expectOutputString("You can't insert a quarter, the machine is sold out<br />");
+    }
+
+    public function testSoldOutStateEjectQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldOutState($machine);
+        $state->ejectQuarter();
+        $this->expectOutputString("You can't eject, you haven't inserted a quarter yet<br />");
+    }
+
+    public function testSoldOutStateTurnCrank()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldOutState($machine);;
+        $state->turnCrank();
+        $this->expectOutputString("You turned but there's no gumballs<br />");
+    }
+
+    public function testSoldOutStateDispense()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldOutState($machine);
+        $state->dispense();
+        $this->expectOutputString("No gumball dispensed<br />");
+    }
+
+    public function testSoldOutStateToString()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldOutState($machine);
+        $this->assertEquals(true, $state->toString() === "sold out");
+    }
+
+    public function testSoldStateInsertQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldState($machine);
+        $state->insertQuarter();
+
+        $this->expectOutputString("Please wait, we're already giving you a gumball<br />");
+    }
+
+    public function testSoldStateEjectQuarter()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldState($machine);
+        $state->ejectQuarter();
+        $this->expectOutputString("Sorry you already turned the crank<br />");
+    }
+
+    public function testSoldStateTurnCrank()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldState($machine);;
+        $state->turnCrank();
+        $this->expectOutputString("Turning twice doesn't get you another gumball<br />");
+    }
+
+    public function testSoldStateDispense()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldState($machine);
+        $state->dispense();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new SoldOutState($machine);
+
+        $this->expectOutputString("Oops, out of gumballs<br />");
+
+        $this->assertEquals(true, $value == $rightValue);
+    }
+
+    public function testSoldStateDispense2()
+    {
+        $machine = new GumballMachine(2);
+        $state = new SoldState($machine);
+        $state->dispense();
+
+        $value = $this->getProtectedProperty($machine, 'state');
+        $rightValue = new NoQuarterState($machine);
+
+        $countValue = $this->getProtectedProperty($machine, 'count');
+        $countRightValue = 1;
+
+        $this->assertEquals(true, $value == $rightValue);
+        $this->assertEquals(true, $countValue == $countRightValue);
+    }
+
+    public function testSoldStateToString()
+    {
+        $machine = new GumballMachine();
+        $state = new SoldState($machine);
+        $this->assertEquals(true, $state->toString() === "delivering a gumball");
+    }
+
+    private function getProtectedProperty($object, $property)
+    {
+        $reflection = new \ReflectionClass($object);
+        $reflectionProperty = $reflection->getProperty($property);
+        $reflectionProperty->setAccessible(true);
+        return $reflectionProperty->getValue($object);
+    }
+}
